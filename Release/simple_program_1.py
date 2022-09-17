bf1a24d7b234b7cf16447de640c8ffd929529203
Choi Chan Woo <ccw23a@outlook.com>
2022-09-17 13:20:23 +0000

    new file:   Learning/__pycache__/input_test.cpython-310-pytest-7.1.3.pyc 	modified:   Release/simple_program_1.py

---
+++ Release/simple_program_1.py
@@ -0,0 +1,12 @@
+import tomli
+
+
+yourname = input("Type Your Name> ")
+
+birthday = input("Type Your 6-digit birthday/year > ")
+
+gender = input("What is your gender? (M/W) > ")
+
+int_b = int(birthday)
+
+print(yourname, int_b, gender)
