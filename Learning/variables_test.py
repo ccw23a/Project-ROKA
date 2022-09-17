365400523cf4f3c619f7dd27f8481de4b8aa8d52
Choi Chan Woo <ccw23a@outlook.com>
2022-09-17 14:01:33 +0000

    modified:   Learning/variables_test.py

---
+++ Learning/variables_test.py
@@ -0,0 +1,14 @@
+int_1 = 10
+str_1 = 'Hello'
+float_1 = 3.14
+bool_1 = True
+
+print(int_1)
+print(str_1)
+print(float_1)
+print(bool_1)
+
+print(type(int_1))
+print(type(str_1))
+print(type(float_1))
+print(type(bool_1))
