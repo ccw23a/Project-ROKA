4a9f227828733e7601e700f9d1e8747f5aef929b
Choi Chan Woo <ccw23a@outlook.com>
2022-09-18 13:32:29 +0000

    modified:   Learning/boolean_logic_test.py

---
+++ Learning/boolean_logic_test.py
@@ -0,0 +1,23 @@
+print(True)
+print(False)
+
+print("\n")
+
+print(10 < 100)
+print(1 > 9)
+
+print("\n")
+
+print(not True)
+print(not False)
+
+print("\n")
+
+print(True and False)
+print(True or False)
+print(False and True)
+print(False or True)
+print(True and True)
+print(True or True)
+print(False and False)
+print(False or False)
