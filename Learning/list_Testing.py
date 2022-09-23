465bbebafb3e8aba1b995a97a6107237fb3d23e6
Choi Chan Woo <ccw23a@outlook.com>
2022-09-23 13:55:13 +0000

    modified:   Learning/list_Testing.py

--- Learning/list_Testing.py
+++ Learning/list_Testing.py
@@ -1,6 +1,35 @@
-test_array = [1,2, True, False, "Hello"] #리스트 선언
+ta1 = [1,2, True, False, "Hello"] #리스트 선언
+ta2 = ["Snake", "Jaguar", False, 5, 9] #리스트 선언
 
-len_a = len(test_array)
+print("ta1 = ", ta1)
+print("ta2 = ", ta2)
+print("\n")
 
-print("length of test_array is ",len_a)
+len_a = len(ta1)
 
+print("length of ta1 is ",len_a)
+
+print("\n")
+
+print("+, *")
+
+print("\n")
+
+print("ta1 + ta2 = ", ta1 + ta2)
+print("ta1 * 2 = ", ta2 * 2)
+
+print("\n")
+
+print("append, insert extend 추가")
+
+ta1.append(True)
+ta1.append("stand")
+
+print(ta1)
+
+ta1.insert(10, 60)
+ta1.insert(0, False)
+
+print(ta1)
+
+print("\n")
