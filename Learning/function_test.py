36ccef234e15d026613723fd305f816a1d9d1b98
Choi Chan Woo <ccw23a@outlook.com>
2022-09-25 14:13:35 +0000

    new file:   Learning/function_test.py

---
+++ Learning/function_test.py
@@ -0,0 +1,29 @@
+def cal_plus(a, b):
+    temp = a + b
+    return temp
+
+print(cal_plus(3, 4))
+
+def cal_minus(a, b):
+    temp = a - b
+    return temp
+
+print(cal_minus(3, 4))
+
+def cal_multi(a, b):
+    temp = a * b
+    return temp
+
+print(cal_multi(3, 4))
+
+def cal_divide(a, b):
+    temp = a / b
+    return temp
+
+print(cal_divide(3, 4))
+
+def cal_dividerem(a, b):
+    temp = a % b
+    return temp
+
+print(cal_dividerem(3, 4))
