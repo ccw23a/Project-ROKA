b536120260430c517b3fb357cf155cbb9cb392ce
Choi Chan Woo <ccw23a@outlook.com>
2022-10-17 13:39:23 +0000

    modified:   Algebra/vector.py 	deleted:    Algorithms/basics.py

--- Algebra/vector.py
+++ Algebra/vector.py
@@ -16,6 +16,18 @@ print("\n")
 
 
 print(np.array_equal(x, y))
-##행렬이 같지 않으므로 False
+##행렬이 같지 않으므로 False (array_equal(행렬1, 행렬2) -> 같은 행렬인지 확인하는 함수)
 
+print("\n")
+print(x+y)
+
+print("\n")
+print(x-y)
+
+##행렬의 합, 행렬의 뺄셈
+
+print("\n")
+print(x*2)
+
+##행렬의 스칼라곱
 
